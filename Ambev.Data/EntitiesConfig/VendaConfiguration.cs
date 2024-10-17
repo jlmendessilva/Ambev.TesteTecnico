e02a965847cdf2a62118e1fe3a931bdf86b2e423
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-17 00:34:35 -0300

    feat: criação dos mapeamentos das classe para gerar a base

---
+++ Ambev.Data/EntitiesConfig/VendaConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Ambev.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace b3digitas.Infra.Data.EntitiesConfiguration
+{
+    public class VendaConfiguration : IEntityTypeConfiguration<Venda>
+    {
+        public void Configure(EntityTypeBuilder<Venda> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Data)
+                .HasColumnType("timestamp without time zone");
+
+            builder.Property(p => p.ClienteId)
+                .IsRequired();
+
+            builder.Property(p => p.ValorTotal)
+                .HasPrecision(10, 2)
+                .IsRequired();
+
+            builder.Property(p => p.Filial)
+                .IsRequired();
+
+            builder.Property(p => p.Cancelado)
+                .IsRequired();
+
+            builder.HasMany(p => p.Itens)
+                .WithOne()
+                .HasForeignKey("VendaId"); 
+
+
+        }
+    }
+}
