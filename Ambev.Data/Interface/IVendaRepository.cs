ce39df7262f0dab757a46c1dc3354cce9af8d84a
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-17 00:35:15 -0300

    feat: criação do repositories

---
+++ Ambev.Data/Interface/IVendaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.Domain;
+
+namespace Ambev.Data.Interfaces
+{
+    public interface IVendaRepository
+    {
+        void Adicionar(Venda venda);
+        Venda ObterPorId(Guid id);
+        void Atualizar(Venda venda);
+        void Remover(Guid id);
+
+    }
+}
