7c28b99118e1a9aabacdf5071c114d0b88022487
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-17 00:36:21 -0300

    feat: criação da DI

---
+++ Ambev.Data/Ioc/DependecyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ambev.Data.Interfaces;
+using Ambev.Data.Repositories;
+
+namespace Ambev.IOC
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration config)
+        {
+
+            services.AddScoped<IVendaRepository, VendaRepository>();
+
+            return services;
+        }
+    }
+}
