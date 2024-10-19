455513214b56055d9aedcfa76c891b4bc6d84c6e
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-18 23:45:06 -0300

    refactor: ajuste de injeçao de dependecia

--- Api/Ambev.API/Program.cs
+++ Api/Ambev.API/Program.cs
@@ -1,3 +1,8 @@
+using Ambev.API.Services;
+using Ambev.API.Services.Interfaces;
+using Ambev.API.Services.Mappings;
+using Ambev.Data.Interfaces;
+using Ambev.Data.Repositories;
 using Ambev.IOC;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddInfra(builder.Configuration);
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(MapEntitiesDto));
+builder.Services.AddScoped<IVendaService, VendaService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
