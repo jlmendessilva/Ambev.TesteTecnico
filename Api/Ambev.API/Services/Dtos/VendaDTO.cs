162caf0ca0aac0ed2f22bbb5869f2028b87ab048
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-19 00:38:20 -0300

    refactor: refatoração pra utilizar nos testes

--- Api/Ambev.API/Services/Dtos/VendaDTO.cs
+++ Api/Ambev.API/Services/Dtos/VendaDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ambev.API.Services.Dtos
 {
-    public record VendaDTO(Guid Id,
+   /* public record VendaDTO(Guid Id,
                            int Numero,
                            DateTime DataCadastro,
                            DateTime DataAtualizacao,
@@ -9,6 +9,20 @@
                            Guid Filial,
                            List<ItemVendaDTO> Itens,
                            bool Cancelado,
-                           bool Finalizada);
+                           bool Finalizada);*/
+
+    public record VendaDTO()
+    {
+        public Guid Id {  get; init; }
+        public int Numero { get; init; }
+        public DateTime DataCadastro { get; init; }
+        public DateTime DataAtualizacao { get; init; }
+        public Guid ClienteId { get; init; }
+        public decimal ValorTotal { get; init; }
+        public Guid Filial { get; init; }
+        public List<ItemVendaDTO> Itens { get; init; } = new List<ItemVendaDTO>();
+        public bool Cancelado { get; init; }
+        public bool Finalizada { get; init; }
+    }
 
 }
