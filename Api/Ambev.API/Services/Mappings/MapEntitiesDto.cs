489b4b674a34c47f0edcf15e7073aaa9b5b23a81
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-18 23:44:11 -0300

    feat: criaçao dos mapeadores e automapper

---
+++ Api/Ambev.API/Services/Mappings/MapEntitiesDto.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.API.Services.Dtos;
+using Ambev.Domain.Entities;
+using AutoMapper;
+
+namespace Ambev.API.Services.Mappings
+{
+    public class MapEntitiesDto : Profile
+    {
+        public MapEntitiesDto()
+        {
+            CreateMap<Venda, VendaDTO>().ReverseMap();
+            CreateMap<ItemVenda, ItemVendaDTO>().ReverseMap();
+        }
+    }
+}
