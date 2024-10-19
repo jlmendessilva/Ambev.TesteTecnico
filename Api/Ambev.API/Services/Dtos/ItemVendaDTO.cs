24747c0dd4b053de69ce41183e097770e67adaed
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-18 23:41:52 -0300

    feat: criação dos DTOs

---
+++ Api/Ambev.API/Services/Dtos/ItemVendaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Ambev.API.Services.Dtos
+{
+    public record ItemVendaDTO(Guid Id, 
+                               Guid VendaId,
+                               Guid ProdutoId,
+                               decimal Quantidade,
+                               decimal ValorUnitario,
+                               decimal Desconto,
+                               decimal ValorTotal);
+
+}
