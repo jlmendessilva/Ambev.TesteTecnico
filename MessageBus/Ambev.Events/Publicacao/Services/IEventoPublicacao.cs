96d75ccbcb81ddb43828e6901cc3605d9c81cb87
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-21 14:01:33 -0300

    feat: implementaçao do servico RabbitMQ

--- MessageBus/Ambev.Events/Publicacao/Services/IEventoPublicacao.cs
+++ MessageBus/Ambev.Events/Publicacao/Services/IEventoPublicacao.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Ambev.MessageBus.Publicacao.Services
 {
     public interface IEventoPublicacao
     {
-        void Publica(object evento);
+        void Publica<T>(string queueName, T evento);
     }
 }
