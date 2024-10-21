731ac300f884aa96cb51e52dc59a0c1f56f4c0a0
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-21 10:09:29 -0300

    feat: criacao de publicador de eventos

---
+++ MessageBus/Ambev.Events/Publicacao/Services/EventoPublicacao.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+
+namespace Ambev.MessageBus.Publicacao.Services
+{
+    public class EventoPublicacao : IEventoPublicacao
+    {
+        private readonly ILogger<EventoPublicacao> _log;
+
+        public EventoPublicacao(ILogger<EventoPublicacao> log)
+        {
+            _log = log;      
+        }
+        public void Publica(object evento)
+        {
+            var eventoJson = JsonSerializer.Serialize(evento);
+            _log.LogInformation($"Evento publicado: {evento.GetType().Name} - {eventoJson}");
+        }
+    }
+}
