f307f4865273797774d34e250f3d905f279498a3
João Luis Mendes <jlmendessilva@gmail.com>
2024-10-21 23:21:44 -0300

    feat: criação do cosumidor de filas rabbitmq

--- ConsumerQueue/Ambev.ConsumerQueue/Program.cs
+++ ConsumerQueue/Ambev.ConsumerQueue/Program.cs
@@ -1,16 +1,37 @@
-﻿
-using Ambev.ConsumerQueue;
+﻿using Ambev.ConsumerQueue.RabbitMQ;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
-Console.WriteLine("Leitura das filas");
-Console.WriteLine("====================");
+namespace Ambev.ConsumerQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
 
-var connectionString = "hostname=localhost;username=guest;password=guest";
-var consumer = new RabbitMQConsumer(connectionString);
+            var consumer = host.Services.GetRequiredService<RabbitMQConsumer>();
+            var filas = new List<string> { "compraCriada", "compraAlterada", "compraCancelada", "ItemCancelado" };
+            consumer.Consume(filas);
+        }
 
-consumer.Consume("compraCriadaFila");
-consumer.Consume("compraAlteradaFila");
-consumer.Consume("compraCanceladaFila");
-consumer.Consume("itemCanceladoFila");
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var rabbitMQConfig = new RabbitMQConfig
+                    {
+                        HostName = "moose.rmq.cloudamqp.com",
+                        UserName = "dyjzckeu",
+                        Password = "Gm1AhedFvjs3qYXoG6a14_DJFaR3RbrR",
+                        VirtualHost = "dyjzckeu",
+                        Port = 5671
+                    };
 
-
-
+                    services.AddSingleton(rabbitMQConfig);
+                    services.AddSingleton<IRabbitMQConnectionFactory, RabbitMQConnectionFactory>();
+                    services.AddSingleton<RabbitMQConsumer>();
+                });
+    }
+}
---
+++ ConsumerQueue/Ambev.ConsumerQueue/RabbitMQ/RabbitMQConnectionFactory.cs
@@ -0,0 +1,36 @@
+﻿using Ambev.ConsumerQueue.RabbitMQ;
+using RabbitMQ.Client;
+
+namespace Ambev.ConsumerQueue
+{
+    public class RabbitMQConnectionFactory : IRabbitMQConnectionFactory
+    {
+        private readonly RabbitMQConfig _config;
+
+        public RabbitMQConnectionFactory(RabbitMQConfig config)
+        {
+            _config = config;
+        }
+
+        public IConnection GetConnection()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _config.HostName,
+                UserName = _config.UserName,
+                Password = _config.Password,
+                VirtualHost = _config.VirtualHost,
+                Port = _config.Port,
+                Ssl = new SslOption
+                {
+                    Enabled = true,
+                    ServerName = _config.HostName
+                }
+            };
+
+            return factory.CreateConnection();
+        }
+    }
+}
+
+
